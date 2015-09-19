4f8e27528bfb1748d7c1c79942b7400f47057da6
steventreed <steventreed@gmail.com>
2015-09-19 16:29:57 -0400

    Initial commit

--- story.rb
+++ story.rb
@@ -12,4 +12,18 @@ require 'pry'
 
 # store the input in a variable
 
+def display_story(user_title, user_rating)
+  puts "#{user_title} #{user_rating.to_s}/10"
+end
+
+puts "Please type the title of a movie:"
+
+user_title = gets.strip
+
+puts "Please type your rating of the movie out of 100:"
+
+user_score = gets.strip
+
+user_rating = user_score.to_i / 10
+
 display_story(user_title, user_rating)
